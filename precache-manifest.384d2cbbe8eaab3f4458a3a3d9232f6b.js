55602a32289a107a10feaccf438654667d86d2f8
kiran <kiran111candy@gmail.com>
2020-05-29 10:14:04 -0400

    Updates

---
+++ precache-manifest.384d2cbbe8eaab3f4458a3a3d9232f6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae65089bd5afb3e9a647dabf2aa4518",
+    "url": "/coronaTracker_react.js/index.html"
+  },
+  {
+    "revision": "418fa3ca998d5967db1b",
+    "url": "/coronaTracker_react.js/static/css/main.702d39c3.chunk.css"
+  },
+  {
+    "revision": "8ac16a1decbbf32a3162",
+    "url": "/coronaTracker_react.js/static/js/2.ce6a21c7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronaTracker_react.js/static/js/2.ce6a21c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418fa3ca998d5967db1b",
+    "url": "/coronaTracker_react.js/static/js/main.58830f6b.chunk.js"
+  },
+  {
+    "revision": "367201c85ba439ed8729",
+    "url": "/coronaTracker_react.js/static/js/runtime-main.eefb8ae4.js"
+  },
+  {
+    "revision": "d8f394721270f6b528abd13b0974516d",
+    "url": "/coronaTracker_react.js/static/media/covid.d8f39472.png"
+  }
+]);
